82245eb9557660452f98f162ec63fac95d50c950
pratamawijaya <set.mnemonix@gmail.com>
2015-07-05 11:57:15 +0700

    update apps

--- apps/AndroidBeginnerCourse/app/src/main/java/com/pratamawijaya/androidbeginnercourse/MainActivity.java
+++ apps/AndroidBeginnerCourse/app/src/main/java/com/pratamawijaya/androidbeginnercourse/MainActivity.java
@@ -1,34 +1,98 @@
 package com.pratamawijaya.androidbeginnercourse;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+// Actvity
+// ActionBarActivity -> secara deafult sudah memiliki actionbar
+// 22.2 -> AppCompatActivity
 
 public class MainActivity extends AppCompatActivity {
 
-  @Override protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
-  }
+    // method itu adalah fungsi
+    // fungsi ada yang mengembalikan nilai dan ada yang TIDAK mengembalikan nilai
 
-  @Override public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
-    return true;
-  }
+    // class  object
+    Button btn1;
 
-  @Override public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
+    EditText inputPhoneNumber;
+    TextView resultPhone;
+    Button btnCall;
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // layout yang dihandle oleh activity
+        setContentView(R.layout.activity_main);
+
+        // intialize
+        // agar bisa digunakan view harus di TypeCasting sesuai dengan objectnya
+        btn1 = (Button) findViewById(R.id.btn1);
+        inputPhoneNumber = (EditText) findViewById(R.id.inputPhoneNumber);
+        resultPhone = (TextView) findViewById(R.id.textPhone);
+        btnCall = (Button) findViewById(R.id.btnCall);
+
+        // event listener
+        btnCall.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // percabangan
+                if (TextUtils.isEmpty(inputPhoneNumber.getText().toString())) {
+                    // jika field phonenumberkosong maka
+                    Toast.makeText(MainActivity.this, "phone number field required", Toast.LENGTH_SHORT).show();
+
+                } else if (inputPhoneNumber.getText().toString().equals("147")) {
+
+                    Toast.makeText(MainActivity.this, "laporan speedy rusak", Toast.LENGTH_SHORT).show();
+
+                } else {
+                    // jika field phonenumber TIDAK kosong maka
+
+                    // untuk mengambil phone number yang berada di edittext
+                    // String phoneNumber = inputPhoneNumber.getText().toString();
+
+                    // set phone number to textView
+                    resultPhone.setText(inputPhoneNumber.getText().toString());
+                }
+
+
+                // finish
+            }
+        });
+
+
     }
 
-    return super.onOptionsItemSelected(item);
-  }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
---
+++ apps/AndroidBeginnerCourse/app/src/main/java/com/pratamawijaya/androidbeginnercourse/SomeActvity.java
@@ -0,0 +1,15 @@
+package com.pratamawijaya.androidbeginnercourse;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+
+/**
+ * Created by pratama on 7/5/15.
+ */
+public class SomeActvity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        setContentView(R.layout.activity_some);
+    }
+}
